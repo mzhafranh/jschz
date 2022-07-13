10236da4b0291245dfa60c0f5eaf8f2b7e351666
mzhafranh <16520139@std.stei.itb.ac.id>
2022-07-13 16:45:49 +0700

    update c19

--- c19/app.js
+++ c19/app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const data = [];
+const fs = require('fs');
+const file_path = "./file.json"
+try{
+    if(fs.existsSync(file_path)){  
+    } else {
+        const data = fs.writeFileSync('file.json', "{}");
+    }
+} catch(err){
+    console.error(err);
+}
+
+const data = JSON.parse(fs.readFileSync('./file.json',{encoding:'utf8', flag:'r'}))
+
+function saveList(arrData){
+    const arrInJson = JSON.stringify(arrData);
+    const data = fs.writeFileSync('file.json', arrInJson);
+}
+
 const app = express();
 const port = 3000;
 
@@ -21,14 +38,15 @@ app.get('/add', (req, res) => {
 })
 
 app.post('/add', (req, res) => {
-    console.log(req.body)
     data.push({string: req.body.string, integer: req.body.integer, float: req.body.float, date: req.body.date, boolean: req.body.boolean})
+    saveList(data);
     res.redirect('/');
 })
 
 app.get('/delete/:id', (req, res) => {
     const index = req.params.id
     data.splice(index, 1);
+    saveList(data);
     res.redirect('/');
 })
 
@@ -38,9 +56,11 @@ app.get('/edit/:id', (req, res) => {
 
 app.post('/edit/:id', (req, res) => {
     data[req.params.id] = {string: req.body.string, integer: req.body.integer, float: req.body.float, date: req.body.date, boolean: req.body.boolean}
+    saveList(data);
     res.redirect('/');
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
