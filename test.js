e7333ee2b0d5562bcb644d2003ce4c51f995f031
mzhafranh <16520139@std.stei.itb.ac.id>
2022-07-27 11:59:23 +0700

    c22 ejs done

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
-const { Pool } = require("pg");
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "c21db",
-  password: "12345",
-  port: 5432,
-});
+// const { Pool } = require("pg");
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "c21db",
+//   password: "12345",
+//   port: 5432,
+// });
 
 
-var sort = 'DESC'
-var sortby = 'id';
+// var sort = 'DESC'
+// var sortby = 'id';
 
-pool.query(`SELECT * FROM data ORDER BY ${sortby} ${sort} LIMIT $1 OFFSET $2`, [5, 0], (err, result) => {
-    if (err) {
-      return console.error('Error executing query', err.stack)
-    }
-    console.log(result.rows) 
-})
+// pool.query(`SELECT * FROM data ORDER BY ${sortby} ${sort} LIMIT $1 OFFSET $2`, [5, 0], (err, result) => {
+//     if (err) {
+//       return console.error('Error executing query', err.stack)
+//     }
+//     console.log(result.rows) 
+// })
 
 // var id = 5;
 // var string = 'SMAN';
@@ -45,3 +45,41 @@ pool.query(`SELECT * FROM data ORDER BY ${sortby} ${sort} LIMIT $1 OFFSET $2`, [
 
 // console.log(sql)
 // console.log(params)
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("test");
+  var query = { string: /SMAN/ };
+  var contohReg = new RegExp('SMAN');
+  console.log(contohReg);
+  var sortByy = 'id'
+  var order = -1
+  var sortMongo = `{"${sortByy}" : ${order}}`
+  // sortMongo += `"${sortByy}"`;
+  // sortMongo += ':';
+  // sortMongo += order;
+  // sortMongo += `}`
+
+
+//   var coba = `{"id" : 2}`
+  // coba = JSON.parse(coba);
+  // console.log(coba)
+//   console.log('itu', sortMongo)
+//   sortMongo = JSON.parse(sortMongo);
+  // console.log('ini', sortMongo)
+  // sortMongo = {'id' : -1}
+  dbo.collection("data").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    console.log(result.length)
+    db.close();
+  });
+});
+
+// var x = "SMAN"
+// var a = {"makanan" : 1}
+// a["minuman"] = `/${x}/`
+// console.log(a);
