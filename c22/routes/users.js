79262dc9b93deec2adb3c8371f77151353f37eab
mzhafranh <16520139@std.stei.itb.ac.id>
2022-07-29 21:05:35 +0700

    c22 rapihin

--- c22/routes/users.js
+++ c22/routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+module.exports = function (db) {
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+ return router;
+}
